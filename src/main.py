88f43cdef8449815682151255bc668d5a42d1faa
Adam Kapica <adam.kapica@gmail.com>
2021-05-24 19:55:39 +0200

    code refactor

--- src/main.py
+++ src/main.py
@@ -1,30 +1,6 @@
-import csv
-
-from sklearn.metrics import precision_recall_fscore_support, accuracy_score
-
 from src.algorithms.lcpc import LCPC
 from src.algorithms.naive_bayes import NaiveBayes
-from src.dataset import Dataset
-
-
-# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_recall_fscore_support.html
-def calc_metrics(true, predicted):
-    prf = precision_recall_fscore_support(true, predicted, average="weighted", zero_division=0)
-    acc = accuracy_score(true, predicted)
-    return prf, acc
-
-
-def save_results(metrics, ds_name, classifier_name):
-    for i in range(len(metrics[0]) - 1):
-        print(ds_name, classifier_name, metrics[0][i])
-    print(ds_name, classifier_name, metrics[1])
-
-
-def create_dataset(ds_path, ds_name):
-    with open(ds_path + ds_name + ".csv") as f:
-        csv_input = csv.reader(f, delimiter=',')
-        return Dataset(csv_input, train_ratio, datasets_info[ds_name], buckets)
-
+from src.utils import create_dataset, train, test, save_results
 
 if __name__ == "__main__":
     dataset_path, train_ratio, buckets = "../data/", 0.8, 10
@@ -38,7 +14,7 @@ if __name__ == "__main__":
     }
 
     for ds_name in datasets_info.keys():
-        dataset = create_dataset(dataset_path, ds_name)
+        dataset = create_dataset(dataset_path, ds_name, train_ratio, datasets_info, buckets)
         # classifiers = [NaiveBayes(), LCPC(), SPRINT()]
         classifiers = [NaiveBayes(), LCPC()]
 
@@ -47,18 +23,7 @@ if __name__ == "__main__":
                 train_ds, test_ds = dataset.get_train_set(buckets=True), dataset.get_test_set(buckets=True)
             else:
                 train_ds, test_ds = dataset.get_train_set(buckets=False), dataset.get_test_set(buckets=False)
-            # train
-            classifier.train(train_ds)
-            # test
-            true, predicted, i = [], [], 0
-            for row in test_ds:
-                i += 1
-                if i == 20:
-                    break
-                sample, gt = row[:-1], row[-1]
-                predicted_class = classifier.predict(sample)
-                true.append(gt), predicted.append(predicted_class)
-            # calc metrics
-            metrics = calc_metrics(true, predicted)
-            # save results
+
+            train(classifier, train_ds)
+            metrics = test(classifier, test_ds)
             save_results(metrics, ds_name, classifier.__class__.__name__)
---
+++ src/utils.py
@@ -0,0 +1,41 @@
+import csv
+
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+
+from src.dataset import Dataset
+
+
+def train(classifier, train_ds):
+    classifier.train(train_ds)
+
+
+def test(classifier, test_ds):
+    true, predicted, i = [], [], 0
+    for row in test_ds:
+        i += 1
+        if i == 30:
+            break
+        sample, gt = row[:-1], row[-1]
+        predicted_class = classifier.predict(sample)
+        true.append(gt), predicted.append(predicted_class)
+
+    return calc_metrics(true, predicted)
+
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_recall_fscore_support.html
+def calc_metrics(true, predicted):
+    prf = precision_recall_fscore_support(true, predicted, zero_division=0)
+    acc = accuracy_score(true, predicted)
+    return prf, acc
+
+
+def save_results(metrics, ds_name, classifier_name):
+    for i in range(len(metrics[0]) - 1):
+        print(ds_name, classifier_name, metrics[0][i])
+    print(ds_name, classifier_name, metrics[1])
+
+
+def create_dataset(ds_path, ds_name, train_ratio, ds_info, buckets):
+    with open(ds_path + ds_name + ".csv") as f:
+        csv_input = csv.reader(f, delimiter=',')
+        return Dataset(csv_input, train_ratio, ds_info[ds_name], buckets)
